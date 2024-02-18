34e4b04322734f9c2eb3ae983129f115d516db2d
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2024-02-18 21:35:09 +0530

    merge

--- frontend/src/components/AllTransactions.jsx
+++ frontend/src/components/AllTransactions.jsx
@@ -32,13 +32,13 @@ function AllTransactions() {
     loading?<Loading/>:<div>
      {(data.length==0)? 
      <div>
-      <Link to={'/dashboard'} className='ml-10 mt-20 p-2 relative top-10 bg-black text-white border-0 rounded-md px-4'>Go Back</Link>
+      <Link to={'/dashboard'} className='ml-20 mt-30 p-2 relative top-15 bg-black text-white border-0 rounded-md px-4'>Go Back</Link>
        <div className='text-4xl font-md text-center flex justify-center items-center h-screen w-full  '>No transactions yet!!</div>
      </div>:(
     <div className={`flex flex-wrap flex-col items-center  justify-between px-4 py-1  `}>
-        <Link to={'/dashboard'}><button className='absolute mt-2 left-5 text-lg bg-black text-white px-5 pb-1 rounded border-0'>Go Back</button></Link> 
+        <Link to={'/dashboard'}><button className='absolute mt-2 left-5 text-lg bg-black text-white px-5 pb-1 rounded border-0'>Go Back</button></Link>  <br />
         <h1 className='block text-3xl text-center font-md mt-10 mb-3 underline'> Your Transactions</h1> <br />
-        <div className='bg-slate-100 mx-10 w-8/12 flex flex-col p-2  justify-center items-center'>
+        <div className='bg-slate-100 mx-10 w-11/12 flex flex-col p-2  justify-center items-center'>
         
         {data.map((ele,i)=>{
           if(ele.to=="Me"){
